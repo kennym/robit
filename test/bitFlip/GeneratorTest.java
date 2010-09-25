a31841ad17836a86154f51c5d47bde641d031615
Kenny Meyer <knny.myer@gmail.com>
2010-09-25 12:13:57 -0400

    [bitFlip] Actualización de las unidades de prueba JUNIT
        83.3% de las pruebas pasan.

--- test/bitFlip/GeneratorTest.java
+++ test/bitFlip/GeneratorTest.java
@@ -93,9 +93,9 @@ public class GeneratorTest {
         Generator testFunc = new Generator();
 
         testFunc.incrementStep();
-        assertEquals(testFunc.getStep(), 2);
+        assertEquals(testFunc.getStep(), 1);
         testFunc.incrementStep();
-        assertEquals(testFunc.getStep(), 3);
+        assertEquals(testFunc.getStep(), 2);
     }
 
     @Test
@@ -127,5 +127,147 @@ public class GeneratorTest {
             testFunc.genRandomBit();
             assertTrue((testFunc.getRandomBit() == 0) || (testFunc.getRandomBit() == 1));
         }
+    }
+
+    /**
+     * Test of getInstance method, of class Generator.
+     */
+    @Test
+    public void testGetInstance() {
+        System.out.println("getInstance");
+        Generator expResult = Generator.getInstance();
+        Generator result = Generator.getInstance();
+        // Comprobar que los dos objetos son los mismos, porque Generator
+        // es un Singleton.
+        assertTrue(expResult == result);
+    }
+
+    /**
+     * Test of clone method, of class Generator.
+     */
+    @Test (expected=CloneNotSupportedException.class)
+    public void testClone() throws Exception {
+        Generator g = Generator.getInstance();
+        g.clone();
+    }
+
+    /**
+     * Test of yes method, of class Generator.
+     */
+    @Test
+    public void testYes() {
+        System.out.println("yes");
+        Generator instance = new Generator();
+        instance.yes();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of no method, of class Generator.
+     */
+    @Test
+    public void testNo() {
+        System.out.println("no");
+        Generator instance = new Generator();
+        instance.no();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of reset method, of class Generator.
+     */
+    @Test
+    public void testReset() {
+        System.out.println("reset");
+        Generator instance = new Generator();
+        instance.reset();
+        assertTrue(instance.getFinalNumber().contains("0"));
+        assertTrue(instance.getStep() == 0);
+    }
+
+    /**
+     * Test of getStep method, of class Generator.
+     */
+    @Test
+    public void testGetStep() {
+        System.out.println("getStep");
+        Generator instance = new Generator();
+        int expResult = 0;
+        int result = instance.getStep();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setRandomBit method, of class Generator.
+     * 
+     * Give the method an invalid value.
+     * 
+     * An invalid value is anything which is not an integer either 0 or 1.
+     */
+    @Test (expected=AssertionError.class)
+    public void testSetRandomBit() throws Exception {
+        System.out.println("setRandomBit");
+        // El valor inválido...
+        int n = -1;
+        Generator instance = new Generator();
+        instance.setRandomBit(n);
+        
+        n = 10;
+        instance.setRandomBit(n);
+    }
+
+    /**
+     * Test of getRandomBit method, of class Generator.
+     */
+    @Test
+    public void testGetRandomBit() {
+        System.out.println("getRandomBit");
+        Generator instance = new Generator();
+        int expResult = 0;
+        int result = instance.getRandomBit();
+        assertEquals(expResult, result);
+        
+        expResult = 1;
+        instance.setRandomBit(1);
+        result = instance.getRandomBit();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of genRandomBit method, of class Generator.
+     */
+    @Test
+    public void testGenRandomBit() {
+        System.out.println("genRandomBit");
+        Generator instance = new Generator();
+        for (int i = 0; i < 10; i++) {
+            instance.genRandomBit();
+            assertTrue(instance.getRandomBit() == 0 || instance.getRandomBit() == 1);
+        }
+    }
+
+    /**
+     * Test of getFinalNumber method, of class Generator.
+     */
+    @Test
+    public void testGetFinalNumber() {
+        System.out.println("getFinalNumber");
+        Generator instance = new Generator();
+        String expResult = "0";
+        String result = instance.getFinalNumber();
+    }
+
+    /**
+     * Test of setFinalNumber method, of class Generator.
+     */
+    @Test
+    public void testSetFinalNumber() {
+        System.out.println("setFinalNumber");
+        int new_value = 0;
+        Generator instance = new Generator();
+        instance.setFinalNumber(new_value);
+        assertEquals(instance.getFinalNumber(), String.valueOf(new_value));
     }
 }
---
+++ test/signMix/GeneratorTest.java
@@ -0,0 +1,106 @@
+package signMix;;
+
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kenny
+ */
+public class GeneratorTest {
+
+    public GeneratorTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of generarNumero method, of class Generator.
+     */
+    @Test
+    public void testGenerarNumero() {
+        System.out.println("generarNumero");
+        Generator instance = new Generator();
+        String expResult = "";
+        String result = instance.generarNumero();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of generarProblema method, of class Generator.
+     */
+    @Test
+    public void testGenerarProblema() {
+        System.out.println("generarProblema");
+        int nivel = 0;
+        Generator instance = new Generator();
+        ArrayList expResult = null;
+        ArrayList result = instance.generarProblema(nivel);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class Generator.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Generator.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of generarNumero method, of class Generator.
+     */
+    @Test
+    public void testGenerarNumero_0args() {
+        System.out.println("generarNumero");
+        Generator instance = new Generator();
+        String expResult = "";
+        String result = instance.generarNumero();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of generarNumero method, of class Generator.
+     */
+    @Test
+    public void testGenerarNumero_int() {
+        System.out.println("generarNumero");
+        int rango = 0;
+        Generator instance = new Generator();
+        String expResult = "";
+        String result = instance.generarNumero(rango);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+}
